a65440af24e139c6e0d0afe3e7078191ba5b8048
valerie356 <159455273+valerie356@users.noreply.github.com>
2026-02-19 13:25:20 +1100

    Add files via upload

---
+++ week_01/face.c
@@ -0,0 +1,37 @@
+// face.c
+//
+// Written by YOUR-NAME (YOUR-ZID),
+// on TODAYS-DATE 
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+#include <stdio.h>
+
+int main(void) {
+    // I want to print a face
+    // printf("~ ~\n");
+    // printf("0 0\n");
+    // printf(" o\n");
+    // printf(" -\n");
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    // Double \\ to print a \ out
+    printf("\\_/\n");
+
+    return 0;
+}
+
